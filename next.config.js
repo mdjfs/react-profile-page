88b4bb8a54847db60960f4eb2a568c783af5bbae
u632763 <mdfuenmayorsoto@teco.com.ar>
2023-09-15 16:07:02 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverRuntimeConfig: {
-        IMGUR_CLIENT_ID: process.env.IMGUR_CLIENT_ID,
-        IMGUR_CLIENT_SECRET: process.env.IMGUR_CLIENT_SECRET
-    }
-}
+  output: "standalone",
+  basePath: "/github-pages",
+  serverRuntimeConfig: {
+    IMGUR_CLIENT_ID: process.env.IMGUR_CLIENT_ID,
+    IMGUR_CLIENT_SECRET: process.env.IMGUR_CLIENT_SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
